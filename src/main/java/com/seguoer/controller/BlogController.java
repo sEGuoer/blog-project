dec34aee590f2269f4007a0b67bd5562d9a28c09
sEGuoer <709074535@qq.com>
2023-10-12 15:29:56 +0800

    简单配置一些网页和工具类

---
+++ src/main/java/com/seguoer/controller/BlogController.java
@@ -0,0 +1,33 @@
+package com.seguoer.controller;
+
+import com.seguoer.method.R;
+import com.seguoer.po.Blog;
+import com.seguoer.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/blog")
+public class BlogController {
+    @Autowired
+    UserService userService;
+
+    @GetMapping
+    R index(@RequestParam(defaultValue = "0")int page, @RequestParam(defaultValue = "0")int perpage) {
+        if (page == 0 && perpage == 0){
+            return R.ok(userService.selectAllBlog());
+        }else {
+            return R.ok(userService.selectBlogByPage(page,perpage));
+        }
+    }
+
+    @PostMapping
+    R store(Blog blog) {
+        return R.ok(userService.addNewBlog(blog));
+    }
+
+    @DeleteMapping("/{id}")
+    R destory(@PathVariable String id) {
+        return R.ok(userService.deleteBlogByID(id));
+    }
+}
